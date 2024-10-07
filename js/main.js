fa6f20b6ef512f66486e0a9cb68dc95053e6324c
Nicolas <lnicolas.jofre@outlook.com>
2024-10-07 20:45:49 -0300

    cambiando botones leer mas

--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
-
 let prevScrollpos = window.pageYOffset;
-let menu = document.querySelector('#menu');
-let menu_bar = document.querySelector('#btn-toggle');
-window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-      if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-      } else {
-        document.getElementById("navbar").style.top = "-100px";
-        menu.classList.remove("btn-togler-event");
-      }
-      prevScrollpos = currentScrollPos;
-    }
+let menu = document.querySelector("#menu");
+let menu_bar = document.querySelector("#btn-toggle");
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+    menu.classList.remove("btn-togler-event");
+  }
+  prevScrollpos = currentScrollPos;
+};
 
-
-  menu_bar.addEventListener('click',function(){
-    menu.classList.toggle('btn-togler-event')
+menu_bar.addEventListener("click", function () {
+  menu.classList.toggle("btn-togler-event");
 });
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
-    menu.classList.toggle('btn-togler-event');
-  }));
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    menu.classList.toggle("btn-togler-event");
+  })
+);
+
+function showHide(){
+    let container = document.getElementById('cont-texto');
+    if(container.style.display == "block"){
+        container.style.display = "none";
+    }
+    else{
+        container.style.display = "block"
+    }
+}
